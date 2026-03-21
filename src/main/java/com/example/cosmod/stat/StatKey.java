f39aa26c3a227e1bd196735d69ef705f1131a6f3
backgun002-pixel <backgun002@gmail.com>
2026-03-21 15:05:59 +0900

    [cosmod] update StatKey.java

--- src/main/java/com/example/cosmod/stat/StatKey.java
+++ src/main/java/com/example/cosmod/stat/StatKey.java
@@ -8,17 +8,7 @@ public class StatKey {
     public static KeyMapping OPEN_STAT;
 
     public static void register() {
-        // SkillKey와 같은 카테고리 재사용
-        KeyMapping.Category cat = null;
-        try {
-            var field = KeyMapping.class.getDeclaredField("CATEGORIES");
-            field.setAccessible(true);
-            @SuppressWarnings("unchecked")
-            var cats = (java.util.Collection<KeyMapping.Category>) field.get(null);
-            if (!cats.isEmpty()) cat = cats.iterator().next();
-        } catch (Exception ignored) {}
-
         OPEN_STAT = KeyBindingHelper.registerKeyBinding(
-            new KeyMapping("key.cosmod.stat", GLFW.GLFW_KEY_O, cat));
+            new KeyMapping("key.cosmod.stat", GLFW.GLFW_KEY_O, "key.categories.misc"));
     }
 }
